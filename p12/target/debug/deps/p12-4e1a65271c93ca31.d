dcf897b83538085722afba401eb887895ffd74fc
sharwantiwari <shrawant@gmail.com>
2025-04-14 12:31:30 +0400

    print change for testing

---
+++ p12/target/debug/deps/p12-4e1a65271c93ca31.d
@@ -0,0 +1,9 @@
+/Users/sharwantiwari/git/rust-training-2025/p12/target/debug/deps/libp12-4e1a65271c93ca31.rmeta: src/main.rs Cargo.toml
+
+/Users/sharwantiwari/git/rust-training-2025/p12/target/debug/deps/p12-4e1a65271c93ca31.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ p12/target/debug/deps/p12-914b02a961c4506f.d
@@ -0,0 +1,9 @@
+/Users/sharwantiwari/git/rust-training-2025/p12/target/debug/deps/libp12-914b02a961c4506f.rmeta: src/main.rs Cargo.toml
+
+/Users/sharwantiwari/git/rust-training-2025/p12/target/debug/deps/p12-914b02a961c4506f.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
